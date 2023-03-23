aac861dd14f55e9c5d216b21c6a5467ba46fabd0
Brook Tarekegn <brooksideas@gmail.com>
2023-03-23 19:46:02 +0300

    post relational fetching

--- script.ts
+++ script.ts
@@ -2,32 +2,42 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    // const user = await prisma.user.findMany({
-    //     where: {
-    //         NOT: [{
-    //             name: { startsWith: "b" },
+    const user = await prisma.user.findMany({
+        // where: {
+        //    writtenPosts: {
+        //     some:{ 
+        //         title: {
+        //             startsWith: "Ca"
+        //         }
+        //     }
+        //    }
+        // },
+        // include:{
+        //    userPreference: true
+        // },
 
-    //         }, { age: { lt: 2 } }
-    //         ],
-    //     },
-    //     // include:{
-    //     //     userPreference: true
-    //     // },
+        // orderBy: {
+        //     age: "asc" //desc
+        // },
+        // take: 2,
+        // skip: 0
+    })
 
-    //     // orderBy: {
-    //     //     age: "asc" //desc
-    //     // },
-    //     // take: 2,
-    //     // skip: 0
-    // })
-
-    // const post = await prisma.post.create({
-    //     data: {
-    //         authorId: "fc67ad2d-1c13-4a79-9a5e-74fa19bffa05",
-    //         title: "IT",
-    //         rating: 7.8,
-    //         favoritedById: "3dc0691c-47a9-4e00-bc84-9abd3f46932a"
-    //     }
+    const post = await prisma.post.findMany({
+        where: {
+            AND: [{ author: { name: { startsWith: "Y" } } }, { rating: { gt: 2 } }]
+        },
+        select:{
+            author: { select: { name: true , email: true}}
+        }
+    })
+    // .create({
+    // data: {
+    //     authorId: "fc67ad2d-1c13-4a79-9a5e-74fa19bffa05",
+    //     title: "IT",
+    //     rating: 7.8,
+    //     favoritedById: "3dc0691c-47a9-4e00-bc84-9abd3f46932a"
+    // }
     // })
     console.log(post);
 }

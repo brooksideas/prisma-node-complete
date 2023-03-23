cc55e99c193a084ee1f9e43804e309c7936cfa52
Brook Tarekegn <brooksideas@gmail.com>
2023-03-23 18:39:06 +0300

    findUnique

--- script.ts
+++ script.ts
@@ -1,28 +1,13 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient({ log: ["query"] })
+const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.user.deleteMany()
-    const user = await prisma.user.create({
-        data: {
-            name: "Ydi",
-            email: "ydis@tdasest.com",
-            age: 24,
-            preferences: {},
-            userPreference: {
-                create: {
-                    userPreference: true
-                }
-            }
+    const user = await prisma.user.findUnique({
+        where:{
+            email: "ydis@tdasest.com"
         },
-        // include:{
-        //     userPreference: true
-        // }
-        select: {
-            name: true,
-            userPreference: {
-                select: { id: true }
-            }
+        include:{
+            userPreference: true
         }
     })
     console.log(user);

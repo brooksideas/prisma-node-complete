cda2daff5e8231b31b27b2621c0cb7c3ca43b82b
Brook Tarekegn <brooksideas@gmail.com>
2023-03-23 18:25:15 +0300

    schema and logs

--- script.ts
+++ script.ts
@@ -1,9 +1,31 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({ log: ["query"] })
 
 async function main() {
-   const user = await prisma.user.findMany();
-   console.log(user);
+    await prisma.user.deleteMany()
+    const user = await prisma.user.create({
+        data: {
+            name: "Ydi",
+            email: "ydis@tdasest.com",
+            age: 24,
+            preferences: {},
+            userPreference: {
+                create: {
+                    userPreference: true
+                }
+            }
+        },
+        // include:{
+        //     userPreference: true
+        // }
+        select: {
+            name: true,
+            userPreference: {
+                select: { id: true }
+            }
+        }
+    })
+    console.log(user);
 }
 
 main()
